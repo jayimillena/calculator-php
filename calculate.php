983949662f9f7313eb2cba3c1813a9a53659ffb0
Jay Millena <jay.millena07@gmail.com>
2024-08-15 19:31:09 +0800

    Ammended calc

--- calculate.php
+++ calculate.php
@@ -10,5 +10,19 @@ if ($_POST['firstNumber'] == '' || $_POST['secondNumber'] == '') {
   if ($operator == 'addition') {
     $sum = $firstNumber + $secondNumber;
     echo "The sum of $firstNumber and $secondNumber is $sum";
-  } 
+  } else if ($operator == 'subtraction') {
+    $difference = $firstNumber - $secondNumber;
+    echo "The difference of $firstNumber and $secondNumber is $difference";
+  } else if ($operator == 'multiplication') {
+    $product = $firstNumber * $secondNumber;
+    echo "The product of $firstNumber and $secondNumber is $product";
+  } else if ($operator == 'division') {
+    if ($secondNumber == 0) {
+      echo "Undefined";
+    } else {
+      $quotient = $firstNumber * $secondNumber;
+      echo "The quotient of $firstNumber and $secondNumber is $quotient";
+    }
+    
+  }
 }
