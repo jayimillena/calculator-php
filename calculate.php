57a979a00de6e3a9790be8ce481c358966741d79
Jay Millena <jay.millena07@gmail.com>
2024-08-15 19:07:04 +0800

    Added calc

--- calculate.php
+++ calculate.php
@@ -1,3 +1,12 @@
 <?php  
 
-var_dump($_POST['firstNumber'] + $_POST['secondNumber']);
+if ($_POST['firstNumber'] == '' || $_POST['secondNumber'] == '')
+{
+  echo "Fields are required";
+}
+else {
+  $firstNumber = $_POST['firstNumber'];
+  $secondNumber = $_POST['secondNumber'];
+
+  echo $firstNumber + $secondNumber;
+}
